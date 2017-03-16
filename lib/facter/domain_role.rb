e4e8ac2a28e7af66bd720e7a44d50292304ca405
superfantasticawesome <hello@superfantasticawesome.com>
2017-03-16 16:10:54 -0400

    Update domain_role.rb

--- lib/facter/domain_role.rb
+++ lib/facter/domain_role.rb
@@ -1,8 +1,8 @@
 Facter.add("domain_role") do
   confine :kernel => :windows
   setcode do
-    no_domain = Facter::Util::Resolution.exec('C:\Windows\system32\WindowsPowerShell\v1.0\powershell.exe -Command ($Env:Computername -eq $Env:Userdomain)')
-    if no_domain == 'True'
+    standalone_server = Facter::Util::Resolution.exec('C:\Windows\system32\WindowsPowerShell\v1.0\powershell.exe -Command ($Env:Computername -eq $Env:Userdomain)')
+    if standalone_server == 'True'
       'standalone_server'
     else
       domain_controller = Facter::Util::Resolution.exec('C:\Windows\system32\WindowsPowerShell\v1.0\powershell.exe -Command (Get-ADDomainController -EA SilentlyContinue).Enabled')
